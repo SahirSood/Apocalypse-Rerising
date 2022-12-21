ba87d29376634010daf476739b73a00db9ee02b2
SahirSood <sahirsood@gmail.com>
2022-12-21 10:28:58 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame
 import os
 import math
 
+WIDTH, HEIGHT = 500,500
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Apocalypse Rerising")
+
+
 PURPLE = (100,88,132)
 BLACK = (0,0,0)
 
@@ -9,8 +15,12 @@ FPS = 60
 VEL = 3
 
 PLAYER_WIDTH, PLAYER_HEIGHT = 62,50
-ZOMBIE_WIDTH, ZOMBIE_HEIGHT = 75, 50
 GUN_WIDTH, GUN_HEIGHT = 10, 4
+
+CROSSHAIR_IMAGE = pygame.image.load(
+    os.path.join('crosshair.png'))
+
+cursor_rect = CROSSHAIR_IMAGE.get_rect()
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, width, height,pos_x,pos_y):
@@ -28,7 +38,10 @@ class Player(pygame.sprite.Sprite):
             self.rect.y   += VEL
         if keys_pressed[pygame.K_d] and self.rect.x + VEL + self.rect.width < WIDTH: #move right
             self.rect.x  += VEL
-   
+    def create_bullet(self):
+        return Bullet(self.rect.x, self.rect.y)
+
+
 
 class Gun(Player):
     def __init__(self,width, height,pos_x,pos_y):
@@ -46,33 +59,23 @@ class Gun(Player):
         angle = math.degrees(math.atan2(-y_diff, x_diff)) - 180
         self.image = pygame.transform.rotate(self.orig_image, angle)
 
-        self.rect = self.image.get_rect(center=player.rect.center)
+    
 
-class Bullet(Player):
-    def __init__(self,width, height,pos_x,pos_y):
-        super().__init__(width, height,pos_x,pos_y)
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,pos_x ,pos_y):
+        super().__init__(pos_x ,pos_y)
         self.image = pygame.image.load(os.path.join('guns', '5 Bullets', '8.png'))
-        self.orig_image = self.image
         self.rect = self.image.get_rect(center=(pos_x,pos_y))
-    # def update(self):
+    def update(self):
+        self.rect.x += 5
+        
         
 
 
-WIDTH, HEIGHT = 500,500
-
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Apocalypse Rerising")
 
 
 
-IDLE_ZOMBIE_IMAGE = pygame.image.load(
-    os.path.join('zombiefiles', 'male', 'Idle (1).png'))
-IDLE_ZOMBIE = pygame.transform.scale(IDLE_ZOMBIE_IMAGE, (ZOMBIE_HEIGHT,ZOMBIE_WIDTH))
 
-CROSSHAIR_IMAGE = pygame.image.load(
-    os.path.join('crosshair.png'))
-
-cursor_rect = CROSSHAIR_IMAGE.get_rect()
 
 
 player = Player(PLAYER_HEIGHT, PLAYER_WIDTH, 300, 100)
@@ -83,10 +86,12 @@ gun = Gun(PLAYER_HEIGHT, PLAYER_WIDTH, 300, 100)
 gun_group = pygame.sprite.Group()
 gun_group.add(gun)
 
+bullet_group = pygame.sprite.Group()
 
 def draw_window():
     WIN.fill(PURPLE)
     pygame.draw.rect(WIN, BLACK, (0, 0, WIDTH, HEIGHT), 10 )
+    bullet_group.draw(WIN)
     player_group.draw(WIN)
     gun_group.draw(WIN)
     WIN.blit(CROSSHAIR_IMAGE, cursor_rect)
@@ -107,9 +112,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                bullet_group.add(player.create_bullet())
         keys_pressed = pygame.key.get_pressed()
         player_group.update(keys_pressed)
-        gun_group.update()        
+        gun_group.update() 
+        bullet_group.update()       
         draw_window()
 
     pygame.quit()
